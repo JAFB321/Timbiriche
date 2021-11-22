42f85571405b19e83bd765cc861fc7db081d421f
JAFB321 <jafb321@gmail.com>
2021-11-21 18:07:31 -0700

    test: remover test de server aislado

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -9,29 +9,29 @@ import Controlador.observer.TestObserver;
 public class Main {
 
     public static void main(String[] args) {
-        //MenuPrincipal form = new MenuPrincipal();
-        //form.setVisible(true);
+        MenuPrincipal form = new MenuPrincipal();
+        form.setVisible(true);
         
         
-        System.out.println("Conexion:");
-        try {
-           Server server = new Server(4008);
-           server.Init();
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-        
-        try {
-            Conexion cliente = new Conexion("localhost", 4008);
-            
-            TestObserver a = new TestObserver();
-            cliente.addObserver(a);
-            
-            cliente.Init();
-            
-        } catch (Exception e) {
-            System.out.println(e);
-        }
+//        System.out.println("Conexion:");
+//        try {
+//           Server server = new Server(4008);
+//           server.Init();
+//        } catch (Exception e) {
+//            System.out.println(e);
+//        }
+//        
+//        try {
+//            Conexion cliente = new Conexion("localhost", 4008);
+//            
+//            TestObserver a = new TestObserver();
+//            cliente.addObserver(a);
+//            
+//            cliente.Init();
+//            
+//        } catch (Exception e) {
+//            System.out.println(e);
+//        }
         
     }
 
