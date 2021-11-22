72e9d8773a24ce75275856deb6aed423cc8e90bb
JAFB321 <jafb321@gmail.com>
2021-11-21 18:03:10 -0700

    feat: clase de handler de peticiones

--- src/Controlador/Sala/cliente/ClientePeticionesHandler.java
+++ src/Controlador/Sala/cliente/ClientePeticionesHandler.java
@@ -1,6 +1,30 @@
 package Controlador.Sala.cliente;
 
+import Controlador.Conexiones.cliente.Conexion;
+import Controlador.Conexiones.server.AccionServer;
+import Controlador.Sala.ControladorSync;
+import Controlador.Sala.server.AccionesServer.*;
+import Controlador.Sala.Sala;
 
 public class ClientePeticionesHandler {
+
+    Conexion conexion;
+    Sala sala;
     
+    // Para poder liberar acciones emitidas una vez recibida la respuesta
+    ControladorSync controladorSync;
+
+    public ClientePeticionesHandler(Conexion conexion, Sala sala, ControladorSync controladorSync) {
+        this.conexion = conexion;
+        this.sala = sala;
+        this.controladorSync = controladorSync;
+    }
+
+    public void handlePeticion(AccionServer accion) {
+
+//        if (accion instanceof SolicitarUnirse) {
+//            añadirJugadorSala(cliente, (SolicitarUnirse) accion);
+//        }
+
+    }
 }
