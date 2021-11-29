bba2b0e7a12b7a47a790ca4bd39e1dc647eb2e80
JAFB321 <jafb321@gmail.com>
2021-11-29 01:50:30 -0700

    feat: Estructura de casilla completada por jugador

---
+++ src/timbiriche/estructuras/Casilla.java
@@ -0,0 +1,37 @@
+
+package Timbiriche.estructuras;
+
+// Representa cuatro Lineas que forman una casilla valida de un jugador
+public class Casilla {
+    Punto puntoA;
+    Punto puntoB;
+    Punto puntoC;
+    Punto puntoD;
+    
+    Jugador jugador;
+
+    public Casilla(Jugador jugador, Punto puntoA, Punto puntoB, Punto puntoC, Punto puntoD) {
+        this.puntoA = puntoA;
+        this.puntoB = puntoB;
+        this.puntoC = puntoC;
+        this.puntoD = puntoD;
+        this.jugador = jugador;
+    }
+
+    public Jugador getJugador() {
+        return jugador;
+    }
+
+    public Punto getPuntoA() {
+        return puntoA;
+    }
+
+    public Punto getPuntoB() {
+        return puntoB;
+    }
+
+    public Punto getPuntoC() {
+        return puntoC;
+    }
+       
+}
