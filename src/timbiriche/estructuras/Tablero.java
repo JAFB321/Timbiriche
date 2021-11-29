da694806f38609791375816a1504c672b04749ab
JAFB321 <jafb321@gmail.com>
2021-11-29 01:56:46 -0700

    feat: Añadir casillas

--- src/timbiriche/estructuras/Tablero.java
+++ src/timbiriche/estructuras/Tablero.java
@@ -8,6 +8,7 @@ public class Tablero implements Serializable{
     private int alto = 10;
     private int ancho = 10;
     private ArrayList<Linea> lineas = new ArrayList<Linea>();
+    private ArrayList<Casilla> casillas = new ArrayList<>();
 
     public Tablero(){}
 
@@ -32,11 +33,36 @@ public class Tablero implements Serializable{
         this.ancho = ancho;
     }
 
-    public void dibujarLinea(Linea linea){
+    /**
+     * Retorna una casilla si la linea completo las 4 lineas de la casilla
+     * Retorna null si no
+     */
+    public Casilla dibujarLinea(Linea linea){
         lineas.add(linea);
+        
+        return null;
     }
 
     public Linea[] getLineas() {
         return lineas.toArray(new Linea[0]);
+    }
+    
+    public boolean estaDisponible(Linea linea){
+        for (Linea l : lineas) {
+            if(l.esIgual(linea)) return false;
+        }
+        
+        return true;
+    }
+    
+    public Casilla formarCasilla(Jugador jugador, Punto puntoA, Punto puntoB, Punto puntoC, Punto puntoD){
+        Casilla casilla = new Casilla(jugador, puntoA, puntoB, puntoC, puntoD);
+        this.casillas.add(casilla);
+        
+        return casilla;
+    }
+    
+    public Casilla[] getCasillas() {
+        return casillas.toArray(new Casilla[0]);
     }
 }
