87aa89b516513c99f58cc93e039ff43c694a0cb6
JAFB321 <jafb321@gmail.com>
2021-12-02 01:28:00 -0700

    feat: Añadir eventos de sala y juego para la UI

--- src/UI/eventos/juego/IGameEventsListener.java
+++ src/UI/eventos/juego/IGameEventsListener.java
@@ -1,6 +1,12 @@
 package UI.eventos.juego;
 
+import Timbiriche.estructuras.Juego;
+import Timbiriche.estructuras.Jugador;
+
 
 public interface IGameEventsListener{
-    // Estos metodos seran implementados por la 
+    // Estos metodos seran implementados por la UI
+    
+    public void on_Error(String msg);
+    public void on_LineaTrazada(Jugador jugador, Juego juegoActualizado);
 }
--- src/UI/eventos/sala/ISalaListener.java
+++ src/UI/eventos/sala/ISalaListener.java
@@ -1,6 +1,7 @@
 package UI.eventos.sala;
 
  // Interface implementada por la UI que escuche eventos del controlador de la sala 
+import Timbiriche.estructuras.Juego;
 import Timbiriche.estructuras.Jugador;
 
 
@@ -8,5 +9,5 @@ public interface ISalaListener {
    
     public void on_JugadorNuevo(Jugador jugador);
     public void on_JugadorAbandono(Jugador jugador);
-    
+    public void on_IniciarJuego(Juego juego);
 }
